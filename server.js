101dc251cc76a120d522af3131bd4b33b3e81708
harpreetsingh116 <harpreetsinghmehra116@gmail.com>
2026-01-07 00:54:15 +0530

    Done

---
+++ server.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import { createCipheriv } from 'crypto';
+import { generateToken04 } from './token.js';
+
+const app = express();
+app.use(express.json());
+
+const appId = 1042883316;
+const ZEGO_SECRET = "35aaa1b6438edd7e612afd34af9e77f5";
+/* ================== TOKEN UTILS ================== */
+
+app.get('/api/:roomId/:userId', (req, res) => {
+  try {
+    const { roomId, userId } = req.params;
+
+    const token = generateToken04(
+      appId,
+      userId,
+      ZEGO_SECRET,
+      3600
+    );
+
+    res.json({token: token });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+/* ================== SERVER ================== */
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Token server running on port ${PORT}`);
+});
