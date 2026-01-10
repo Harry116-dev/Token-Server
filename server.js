5dcc1a4e255bb4c38d9a0a8772d9e5af6e38b7c3
harpreetsingho7 <82938144+harpreetsingho7@users.noreply.github.com>
2026-01-10 10:52:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ import cors from 'cors';
 const app = express();
 app.use(express.json());
 app.use(cors());
-// const appId = 1042883316;
-// const ZEGO_SECRET = "35aaa1b6438edd7e612afd34af9e77f5";
+const appId = 1042883316;
+const ZEGO_SECRET = "35aaa1b6438edd7e612afd34af9e77f5";
 
-const appId = 449016883;
-const ZEGO_SECRET = "56c44010f2a514fbafa32763c874bde3";
+// const appId = 449016883;
+// const ZEGO_SECRET = "56c44010f2a514fbafa32763c874bde3";
 /* ================== TOKEN UTILS ================== */
 
 app.get('/api/:roomId/:userId', (req, res) => {
